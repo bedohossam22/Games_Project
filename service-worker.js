a0b877ccd67712d30e7677858443490e1ab72a3e
abdelrahman hossam <119519513+bedohossam22@users.noreply.github.com>
2025-07-07 14:15:49 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "./index.html",
+  "./style/style.css",
+  "./js/js.js",
+  "./js/questions.json",
+  "./images/rocket.png",
+  "./images/rocket.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
